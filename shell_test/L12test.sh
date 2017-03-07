0ce7caa1790fcf4b96953f06e8efb79fd0609cc7
popexizhi@99 <popexizhi@gmail.com>
2017-03-07 12:33:28 +0800

    init sh

---
+++ shell_test/L12test.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+hostip=$1
+fp="."
+command_str_fgw="${fp}/fgw_server -cfg=alone.cfg -relay"
+command_str_bgw="${fp}/bgw_server -cfg=alone.cfg -relay"
+command_str_app="${fp}/app_server -cfg=alone.cfg -alone"
+command_str_ue="${fp}/slim_engine_test -cfg=alone.cfg -host=200"
+command_str_ueII="${fp}/slim_engine_test -cfg=alone.cfg -host=201"
+command_c2s_ue="${fp}/slim_engine_test -cfg=alone.cfg  -data_direction=c2s -host=300"
+command_c2s_ueII="${fp}/slim_engine_test -cfg=alone.cfg  -data_direction=c2s -host=301"
+command_udp_ue="${fp}/slim_engine_test -cfg=alone.cfg -host=1200 -udp"
+command_udp_ueII="${fp}/slim_engine_test -cfg=alone.cfg -host=1201 -udp"
+command_udp_c2s_ue="${fp}/slim_engine_test -cfg=alone.cfg  -data_direction=c2s -host=1300 -case=UdpThroughputTest.cfg"
+command_udp_c2s_ueII="${fp}/slim_engine_test -cfg=alone.cfg  -data_direction=c2s -host=1301 -case=UdpThroughputTest.cfg"  
+
+rm -rf *.db
+rm -rf nohup.out
+mv *.log.txt backup
+mv -rf log backup
+
+
+echo "starttest**********************"
+nohup ${command_str_fgw} &
+fgw_pid="$!"
+echo "fgw pid: " $fgw_pid
+sleep 6
+nohup ${command_str_bgw} &
+bgw_pid="$!"
+echo "bgw pid: " $bgw_pid
+sleep 3
+nohup ${command_str_app} &
+app_pid="$!"
+echo "app pid: " $app_pid
+sleep 2
+nohup ${command_str_ue} &
+ue_pid="$!"
+echo "ue pid: "$ue_pid
+sleep 300
+echo "stop all test**********************"
+kill -9 ${fgw_pid} ${bgw_pid} ${app_pid} ${ue_pid}
---
+++ shell_test/testcase.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+i=$1
+case $i in 
+"fgw")
+    echo "testcase 1"
+    ;;
+"bgw")
+    echo "testcase 2"
+    ;;
+*)
+    echo "testcase *"
+    ;;
+esac
